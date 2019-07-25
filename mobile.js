d93cc3466820043171153c2e07716b453c04e688
Amer Albareedi <aalbareedi@yahoo.com>
2019-07-25 00:03:51 -0500

    javascript functionality

--- mobile.js
+++ mobile.js
@@ -14,41 +14,55 @@ let contactFormCancelBtn = document.getElementById("formCancelBtn");
 let contactFromBackArrow = document.getElementById("backArrow");
 let contactFormButtonsBar = document.getElementById("contactFormButtons");
 let formInputFields = document.querySelectorAll("input, textarea");
+let backToTopBtn = document.getElementById("backToTopBtn");
+
+function scrollBodyToTop() {
+  if (document.documentElement.scrollTop > 0) {
+    document.documentElement.scrollTop =
+      document.documentElement.scrollTop * 0.7;
+
+    setTimeout(function() {
+      scrollBodyToTop();
+    }, 40);
+  }
+}
 
 for (let i = 0; i < formInputFields.length; i++) {
+  // onfocus: when input field is selected
   formInputFields[i].onfocus = function() {
-    // this.getBoundingClientRect().top -
-    // contactForm.getBoundingClientRect().top;
-
-    // console.log("textbox");
-    // console.log(this.getBoundingClientRect().top);
-    // console.log("form");
-    // console.log(contactForm.getBoundingClientRect().top);
-    // console.log("this");
-    // console.log(this);
-
     let currentInput = this;
 
     setTimeout(function() {
+      // .getBoundingClientRect().top gives distance from element to top of viewport
+      let inputDistanceFromTop = currentInput.getBoundingClientRect().top;
       if (currentInput.tagName == "TEXTAREA") {
-        contactForm.scrollTop += currentInput.getBoundingClientRect().top - 104;
+        // contactForm.scrollTop: how far contactForm is scrolled, 0 initially
+        contactForm.scrollTop += inputDistanceFromTop - 104;
       } else {
-        contactForm.scrollTop += currentInput.getBoundingClientRect().top - 164;
+        contactForm.scrollTop += inputDistanceFromTop - 164;
       }
     }, 10);
 
     contactFormButtonsBar.classList.add("displayHidden");
     mainDock.classList.add("displayHidden");
   };
-
+  // onblur: when leaving an input field
   formInputFields[i].onblur = function() {
     contactFormButtonsBar.classList.remove("displayHidden");
     mainDock.classList.remove("displayHidden");
   };
 }
 
+if (document.documentElement.scrollTop > 0) {
+  backToTopBtn.classList.remove("displayHidden");
+}
+
 mainNavBar.onclick = function() {
-  body.scrollTop = 0;
+  // body.scrollTop = 0;
+  // workaround for Chrome scroll to top
+  // document.documentElement.scrollTop = 0;
+
+  scrollBodyToTop();
 };
 
 navToggleBtn.onclick = function(event) {
@@ -85,4 +99,16 @@ contactFromBackArrow.onclick = function() {
   html.classList.remove("overflowHidden");
   contactForm.classList.add("displayHidden");
   body.classList.remove("overflowHidden");
+};
+
+body.onscroll = function() {
+  if (document.documentElement.scrollTop > 0) {
+    backToTopBtn.classList.remove("displayHidden");
+  } else {
+    backToTopBtn.classList.add("displayHidden");
+  }
+};
+
+backToTopBtn.onclick = function() {
+  scrollBodyToTop();
 };
