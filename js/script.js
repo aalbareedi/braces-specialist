02b2d42927a4672fbc4761cfe118ee99f8c1582e
Amer Albareedi <aalbareedi@yahoo.com>
2019-11-04 15:39:09 -0600

    javascript functionality

--- js/script.js
+++ js/script.js
@@ -18,6 +18,16 @@ for (let i = 0; i < navMenuOpt.length; i++) {
 }
 
 $("#reviewSlider").slick({ dots: true, arrows: false });
+/**
+ * Slick dots sticky touch hover workaround - JS portion (also see CSS portion)
+ * * https://github.com/kenwheeler/slick/issues/1945#issuecomment-230158398
+ * * http://stackoverflow.com/a/4734092/4747661
+ *
+ * Marks the slick-dots container as a touch-device so CSS can work it's magic
+ */
+// if ("ontouchstart" in document) {
+this.$(".slick-dots").addClass("touch-device");
+// }
 
 // setTimeout(function() {
 //   alert(window.innerWidth + "," + window.innerHeight);
