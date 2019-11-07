669f8b4cd00219bf306cacd241db37e6e0818324
Amer Albareedi <aalbareedi@yahoo.com>
2019-11-07 02:32:42 -0600

    mobile functionality

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -143,19 +143,19 @@ for (let i = 0; i < formInputFields.length; i++) {
 
   // onfocus: when input field is selected
   formInputFields[i].onfocus = function(event) {
-    // event.currentTarget.style.backgroundColor = "red";
-    // let currentInput = this;
+    event.currentTarget.style.backgroundColor = "red";
+    let currentInput = this;
 
-    // setTimeout(function() {
-    //   // .getBoundingClientRect().top gives distance from element to top of viewport
-    //   let inputDistanceFromTop = currentInput.getBoundingClientRect().top;
-    //   if (currentInput.tagName == "TEXTAREA") {
-    //     // contactForm.scrollTop: how far contactForm is scrolled, 0 initially
-    //     contactForm.scrollTop += inputDistanceFromTop - 104;
-    //   } else {
-    //     contactForm.scrollTop += inputDistanceFromTop - 214;
-    //   }
-    // }, 100);
+    setTimeout(function() {
+      // .getBoundingClientRect().top gives distance from element to top of viewport
+      let inputDistanceFromTop = currentInput.getBoundingClientRect().top;
+      if (currentInput.tagName == "TEXTAREA") {
+        // contactForm.scrollTop: how far contactForm is scrolled, 0 initially
+        contactForm.scrollTop += inputDistanceFromTop - 104;
+      } else {
+        contactForm.scrollTop += inputDistanceFromTop - 214;
+      }
+    }, 100);
 
     contactFormButtonsBar.classList.add("displayHidden");
     mainDock.classList.add("displayHidden");
