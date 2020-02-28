16f31ec899e2be91eaad4c30be29989a67c1e0d0
Amer Albareedi <aalbareedi@yahoo.com>
2020-02-28 00:46:26 -0600

    mobile functionality

--- js/eventHandlers.js
+++ js/eventHandlers.js
@@ -83,7 +83,7 @@ body.onscroll = function() {
   // console.log(statCases.offsetTop);
   // position property affects statYear.offsetTop
   if (
-    scrollingElement.scrollTop + screen.height - 180 >
+    scrollingElement.scrollTop + screen.height - 160 >
     getAbsoluteOffsetTop(statYear)
   ) {
     if (statYear.innerHTML == 1) {
@@ -92,7 +92,7 @@ body.onscroll = function() {
   }
 
   if (
-    scrollingElement.scrollTop + screen.height - 180 >
+    scrollingElement.scrollTop + screen.height - 160 >
     getAbsoluteOffsetTop(statCases)
   ) {
     if (statCases.innerHTML == 1) {
@@ -101,7 +101,7 @@ body.onscroll = function() {
   }
 
   if (
-    scrollingElement.scrollTop + screen.height - 180 >
+    scrollingElement.scrollTop + screen.height - 160 >
     getAbsoluteOffsetTop(statPercent)
   ) {
     if (statPercent.innerHTML == 1) {
